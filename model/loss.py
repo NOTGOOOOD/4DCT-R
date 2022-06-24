8076f97b179b3a7c522a8609b4b189f0f212fda7
xxf <x837790157@qq.com>
2022-06-24 22:44:12 +0800

    MSE and DICE

--- model/loss.py
+++ model/loss.py
@@ -114,6 +114,29 @@ def smooth_loss(disp, image):
     return loss
 
 
+class MSE:
+    """
+    Mean squared error loss.
+    """
+
+    def loss(self, y_true, y_pred):
+        return torch.mean((y_true - y_pred) ** 2)
+
+
+class Dice:
+    """
+    N-D dice for segmentation
+    """
+
+    def loss(self, y_true, y_pred):
+        ndims = len(list(y_pred.size())) - 2
+        vol_axes = list(range(2, ndims + 2))
+        top = 2 * (y_true * y_pred).sum(dim=vol_axes)
+        bottom = torch.clamp((y_true + y_pred).sum(dim=vol_axes), min=1e-5)
+        dice = torch.mean(top / bottom)
+        return -dice
+
+
 if __name__ == "__main__":
     ncc_loss = NCC(3, 5)
     a1 = np.random.rand(10, 1, 90, 150, 150)
