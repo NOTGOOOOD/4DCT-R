146e03e88ab7cb392da3884fcbd63ca12a275a27
xxf <x837790157@qq.com>
2022-06-30 19:47:53 +0800

    窗宽窗位

--- process/processing.py
+++ process/processing.py
@@ -3,6 +3,7 @@ import SimpleITK as sitk
 import numpy as np
 import utils.utilize as ut
 from matplotlib import pyplot as plt
+
 # import ants
 
 # DIRLAB 4DCT 1-10例的 z y x
@@ -37,7 +38,7 @@ def window(img):
     return img.astype(np.uint8)
 
 
-def imgTomhd(file_folder, datatype, shape):
+def imgTomhd(file_folder, datatype, shape, case):
     for file_name in os.listdir(file_folder):
         file_path = os.path.join(file_folder, file_name)
         file = np.memmap(file_path, dtype=datatype, mode='r')
@@ -48,7 +49,7 @@ def imgTomhd(file_folder, datatype, shape):
 
         img = sitk.GetImageFromArray(file)
 
-        target_path = os.path.join(os.path.dirname(file_folder), "Images_mhd")
+        target_path = os.path.join(os.path.dirname(os.path.dirname(file_folder)), f"Case{case}_mhd")
         if not os.path.exists(target_path):
             os.mkdir(os.path.join(target_path))
 
@@ -62,12 +63,6 @@ def imgTomhd(file_folder, datatype, shape):
 def data_standardization_0_255(img):
     array = 255 * (img - np.min(img)) / (np.max(img) - np.min(img))
     return array
-    # ymax = 255
-    # ymin = 0
-    # xmax = max(map(max, img))  # 进行两次求max值
-    # xmin = min(map(min, img))
-    # img_standardization_0_255 = np.round((ymax - ymin) * (img - xmin) / (xmax - xmin) + ymin)
-    # return img_standardization_0_255
 
 
 # def affiine(move_img, fix_img, save_path):
@@ -121,12 +116,11 @@ def read_mhd(mhd_dir):
 
 
 if __name__ == '__main__':
-    print(case_cfg.items())
     project_folder = ut.get_project_path("4DCT").split("4DCT")[0]
-    read_mhd(os.path.join(project_folder, f'datasets/dirlab/Case1Pack/Images_mhd'))
+    # read_mhd(os.path.join(project_folder, f'datasets/dirlab/Case1Pack/Images_mhd'))
 
-    # for item in case_cfg.items():
-    #     case = item[0]
-    #     shape = item[1]
-    #     img_path = os.path.join(project_folder, f'datasets/dirlab/Case{case}Pack/Images')
-    #     imgTomhd(img_path, np.float16, shape)
+    for item in case_cfg.items():
+        case = item[0]
+        shape = item[1]
+        img_path = os.path.join(project_folder, f'datasets/dirlab/Case{case}Pack/Images')
+        imgTomhd(img_path, np.float16, shape, case)
