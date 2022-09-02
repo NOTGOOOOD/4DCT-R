b26c8d9c68cefcd6babd86085fd47fd7c99a50d0
Aflexiblebox <x837790157@qq.com>
2022-09-02 14:32:11 +0800

    医学图像重采样函数

--- process/processing.py
+++ process/processing.py
@@ -121,6 +121,23 @@ def read_mhd(mhd_dir):
         plt.show()
         break
 
+def resize_image_itk(itkimage, newSize, resamplemethod=sitk.sitkNearestNeighbor):
+
+    resampler = sitk.ResampleImageFilter()
+    originSize = itkimage.GetSize()  # 原来的体素块尺寸
+    originSpacing = itkimage.GetSpacing()
+    newSize = np.array(newSize,float)
+    factor = originSize / newSize
+    newSpacing = originSpacing * factor
+    newSize = newSize.astype(np.int)  #spacing肯定不能是整数
+    resampler.SetReferenceImage(itkimage)  # 需要重新采样的目标图像
+    resampler.SetSize(newSize.tolist())
+    resampler.SetOutputSpacing(newSpacing.tolist())
+    resampler.SetTransform(sitk.Transform(3, sitk.sitkIdentity))
+    resampler.SetInterpolator(resamplemethod)
+    itkimgResampled = resampler.Execute(itkimage)  # 得到重新采样后的图像
+    return itkimgResampled
+
 
 if __name__ == '__main__':
     project_folder = ut.get_project_path("4DCT").split("4DCT")[0]
