5b5cdf8f8e8cc87b8696b939ec854091e3a67157
xxf <x837790157@qq.com>
2022-11-30 13:26:05 +0800

    testdataset

--- voxelmorph/datagenerators.py
+++ voxelmorph/datagenerators.py
@@ -1,5 +1,3 @@
-import os
-import glob
 import numpy as np
 import SimpleITK as sitk
 import torch.utils.data as Data
@@ -29,5 +27,28 @@ class Dataset(Data.Dataset):
 
         f_img = sitk.GetArrayFromImage(sitk.ReadImage(self.fixed_files[index]))[np.newaxis, ...]
         f_img = data_standardization_0_n(1, f_img)
-        # 返回值自动转换为torch的tensor类型
-        return m_img, f_img
+
+        return [m_img, self.moving_files[index]], [f_img, self.fixed_files[index]]
+
+
+class TestDataset(Data.Dataset):
+    def __init__(self, moving_files, fixed_files, landmark_files):
+        # 初始化
+        self.moving_files = moving_files
+        self.fixed_files = fixed_files
+        self.landmark_files = landmark_files
+
+    def __len__(self):
+        # 返回数据集的大小
+        return len(self.moving_files)
+
+    def __getitem__(self, index):
+        # 索引数据集中的某个数据，还可以对数据进行预处理
+        # 下标index参数是必须有的，名字任意
+        m_img = sitk.GetArrayFromImage(sitk.ReadImage(self.moving_files[index]))[np.newaxis, ...]
+        m_img = data_standardization_0_n(1, m_img)
+
+        f_img = sitk.GetArrayFromImage(sitk.ReadImage(self.fixed_files[index]))[np.newaxis, ...]
+        f_img = data_standardization_0_n(1, f_img)
+
+        return [m_img, self.moving_files[index]], [f_img, self.fixed_files[index]], self.landmark_files[index]
