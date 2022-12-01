a114031d05370a100cc48dd32bb0ab8f40720b27
xxf <x837790157@qq.com>
2022-12-01 13:56:30 +0800

    use ncc with numerical stablility

--- voxelmorph/losses.py
+++ voxelmorph/losses.py
@@ -6,11 +6,72 @@ Losses for VoxelMorph
 
 import math
 import torch
+import torch.nn as nn
 import numpy as np
 from voxelmorph.config import get_args
 import torch.nn.functional as F
 
 args = get_args()
+
+
+class NCC(nn.Module):
+    '''
+    Calculate local normalized cross-correlation coefficient between tow images.
+    Parameters
+    ----------
+    dim : int
+        Dimension of the input images.
+    windows_size : int
+        Side length of the square window to calculate the local NCC.
+    '''
+
+    def __init__(self, dim, windows_size=11):
+        super().__init__()
+        assert dim in (2, 3)
+        self.dim = dim
+        self.num_stab_const = 1e-4  # numerical stability constant
+
+        self.windows_size = windows_size
+
+        self.pad = windows_size // 2
+        self.window_volume = windows_size ** self.dim
+        if self.dim == 2:
+            self.conv = F.conv2d
+        elif self.dim == 3:
+            self.conv = F.conv3d
+
+    def forward(self, I, J):
+        '''
+        Parameters
+        ----------
+        I and J : (n, 1, h, w) or (n, 1, d, h, w)
+            Torch tensor of same shape. The number of image in the first dimension can be different, in which broadcasting will be used.
+        windows_size : int
+            Side length of the square window to calculate the local NCC.
+
+        Returns
+        -------
+        NCC : scalar
+            Average local normalized cross-correlation coefficient.
+        '''
+        try:
+            I_sum = self.conv(I, self.sum_filter, padding=self.pad)
+        except:
+            self.sum_filter = torch.ones([1, 1] + [self.windows_size, ] * self.dim, dtype=I.dtype, device=I.device)
+            I_sum = self.conv(I, self.sum_filter, padding=self.pad)
+
+        J_sum = self.conv(J, self.sum_filter, padding=self.pad)  # (n, 1, h, w) or (n, 1, d, h, w)
+        I2_sum = self.conv(I * I, self.sum_filter, padding=self.pad)
+        J2_sum = self.conv(J * J, self.sum_filter, padding=self.pad)
+        IJ_sum = self.conv(I * J, self.sum_filter, padding=self.pad)
+
+        cross = torch.clamp(IJ_sum - I_sum * J_sum / self.window_volume, min=self.num_stab_const)
+        I_var = torch.clamp(I2_sum - I_sum ** 2 / self.window_volume, min=self.num_stab_const)
+        J_var = torch.clamp(J2_sum - J_sum ** 2 / self.window_volume, min=self.num_stab_const)
+
+        cc = cross / ((I_var * J_var) ** 0.5)
+
+        return -torch.mean(cc)
 
 # 平滑正则损失
 def gradient_loss(s, penalty='l2'):
