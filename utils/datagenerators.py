f31cab33c7d2da642d477b3f1ea42f9b972da067
xxf <x837790157@qq.com>
2023-03-04 18:00:03 +0800

    patient_dataset

--- utils/datagenerators.py
+++ utils/datagenerators.py
@@ -44,9 +44,8 @@ class Dataset(Data.Dataset):
         return [[m_img, m_name], [f_img, f_name]]
 
 
-class TestDataset(Data.Dataset):
+class DirLabDataset(Data.Dataset):
     def __init__(self, moving_files, fixed_files, landmark_files=None):
-        # 初始化
         self.moving_files = moving_files
         self.fixed_files = fixed_files
         self.landmark_files = landmark_files
@@ -74,3 +73,33 @@ class TestDataset(Data.Dataset):
             raise ValueError
 
         return m_img, f_img, self.landmark_files[index], m_name
+
+
+class PatientDataset(Data.Dataset):
+    def __init__(self, moving_files, fixed_files):
+        self.moving_files = moving_files
+        self.fixed_files = fixed_files
+
+    def __len__(self):
+        return len(self.moving_files)
+
+    def __getitem__(self, index):
+        m_img = sitk.GetArrayFromImage(sitk.ReadImage(self.moving_files[index]))[np.newaxis, ...]
+        m_img = data_standardization_0_n(1, m_img)
+
+        f_img = sitk.GetArrayFromImage(sitk.ReadImage(self.fixed_files[index]))[np.newaxis, ...]
+        f_img = data_standardization_0_n(1, f_img)
+
+        m_name = self.moving_files[index].split('moving\\')[1] if platform.system().lower() == 'windows' else \
+            self.moving_files[index].split('moving/')[1]
+        f_name = self.fixed_files[index].split('fixed\\')[1] if platform.system().lower() == 'windows' else \
+            self.fixed_files[index].split('fixed/')[1]
+
+        if m_name != f_name:
+            print("=================================================")
+            print("{} is not match {}".format(m_name, f_name))
+            print("=================================================")
+            raise ValueError
+
+        return m_img, f_img, m_name
+
