06c3c6545dc819677eb4c0f622fef84fc061bee1
xxf <x837790157@qq.com>
2023-11-22 12:50:17 +0800

    CRegNet

--- lapirn/Code/CRegNet.py
+++ lapirn/Code/CRegNet.py
@@ -130,7 +130,7 @@ class CRegNet_lv0(nn.Module):
 
 
 class CRegNet_lv1(nn.Module):
-    def __init__(self, in_channel, n_classes, start_channel, is_train=True, range_flow=0.4, grid=None, model_lvl0=None):
+    def __init__(self, in_channel, n_classes, start_channel, is_train=True, range_flow=0.4, grid=None, model_lvl0=None, att=False):
         super(CRegNet_lv1, self).__init__()
         self.in_channel = in_channel
         self.n_classes = n_classes
@@ -138,7 +138,7 @@ class CRegNet_lv1(nn.Module):
         self.model_lv0 = model_lvl0
         self.range_flow = range_flow
         self.is_train = is_train
-
+        self.att = att
         self.grid_1 = grid
         self.transform = AdaptiveSpatialTransformer()
 
@@ -159,7 +159,7 @@ class CRegNet_lv1(nn.Module):
         self.down_avg = nn.AvgPool3d(kernel_size=3, stride=2, padding=1, count_include_pad=False)
 
         # self.sa_module = Self_Attn(self.start_channel * 8, self.start_channel * 8)
-        if model_lvl0 is not None:
+        if model_lvl0 is not None and att:
             self.ca_module = across_atn(self.start_channel * 4, self.start_channel * 4)
 
         self.decoder = nn.Sequential(
@@ -212,7 +212,8 @@ class CRegNet_lv1(nn.Module):
         output_disp_e0_v = self.output_lvl1(decoder) * self.range_flow
         if self.model_lv0 is not None:
             output_disp_e0_v += lvl0_disp_up
-            output_disp_e0_v += self.ca_module(fea_e0, lvl0_embedding, output_disp_e0_v)
+            if self.att:
+                output_disp_e0_v += self.ca_module(fea_e0, lvl0_embedding, output_disp_e0_v)
         warpped_inputx_lvl1_out = self.transform(down_x, output_disp_e0_v.permute(0, 2, 3, 4, 1),
                                                  self.grid_1.get_grid(down_x.shape[2:], True))
 
@@ -224,12 +225,12 @@ class CRegNet_lv1(nn.Module):
 
 class CRegNet_lv2(nn.Module):
     def __init__(self, in_channel, n_classes, start_channel, is_train=True, range_flow=0.4,
-                 model_lvl1=None, grid=None):
+                 model_lvl1=None, grid=None, att=False):
         super(CRegNet_lv2, self).__init__()
         self.in_channel = in_channel
         self.n_classes = n_classes
         self.start_channel = start_channel
-
+        self.att = att
         self.range_flow = range_flow
         self.is_train = is_train
 
@@ -257,7 +258,7 @@ class CRegNet_lv2(nn.Module):
         self.down_avg = nn.AvgPool3d(kernel_size=3, stride=2, padding=1, count_include_pad=False)
 
         # self.sa_module = Self_Attn(self.start_channel * 8, self.start_channel * 8)
-        if model_lvl1 is not None:
+        if model_lvl1 is not None and att:
             self.ca_module = across_atn(self.start_channel * 4, self.start_channel * 4)
 
         self.decoder = nn.Sequential(
@@ -319,7 +320,8 @@ class CRegNet_lv2(nn.Module):
         output_disp_e0_v = self.output_lvl2(decoder) * self.range_flow
         if self.model_lvl1 is not None:
             compose_field_e0_lvl2 = output_disp_e0_v + lvl1_disp_up
-            compose_field_e0_lvl2 += self.ca_module(fea_e0, lvl1_embedding, output_disp_e0_v)
+            if self.att:
+                compose_field_e0_lvl2 += self.ca_module(fea_e0, lvl1_embedding, output_disp_e0_v)
         else:
             compose_field_e0_lvl2 = output_disp_e0_v
 
@@ -336,12 +338,12 @@ class CRegNet_lv2(nn.Module):
 
 class CRegNet_lv3(nn.Module):
     def __init__(self, in_channel, n_classes, start_channel, is_train=True, range_flow=0.4,
-                 model_lvl2=None, grid=None):
+                 model_lvl2=None, grid=None, att=False):
         super(CRegNet_lv3, self).__init__()
         self.in_channel = in_channel
         self.n_classes = n_classes
         self.start_channel = start_channel
-
+        self.att = att
         self.range_flow = range_flow
         self.is_train = is_train
 
@@ -367,7 +369,7 @@ class CRegNet_lv3(nn.Module):
                                      padding=0, output_padding=0, bias=bias_opt)
 
         # self.sa_module = Self_Attn(self.start_channel * 8, self.start_channel * 8)
-        if model_lvl2 is not None:
+        if model_lvl2 is not None and att:
             self.ca_module = across_atn(self.start_channel * 4, self.start_channel * 4)
 
         self.decoder = nn.Sequential(
@@ -428,7 +430,8 @@ class CRegNet_lv3(nn.Module):
         output_disp_e0_v = self.output_lvl3(decoder) * self.range_flow
         if self.model_lvl2 is not None:
             compose_field_e0_lvl1 = output_disp_e0_v + lvl2_disp_up
-            compose_field_e0_lvl1 += self.ca_module(fea_e0, lvl2_embedding, output_disp_e0_v)
+            if self.att:
+                compose_field_e0_lvl1 += self.ca_module(fea_e0, lvl2_embedding, output_disp_e0_v)
         else:
             compose_field_e0_lvl1 = output_disp_e0_v
 
